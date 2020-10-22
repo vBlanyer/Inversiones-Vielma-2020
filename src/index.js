a603410a3b33bc140300a83a40eefb3def99f05b
Blanyer Vielma <vblanyer@gmail.com>
2020-10-21 21:50:55 -0400

    configurando para heroku

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ mongoose.connect('mongodb://localhost/inversionesVielma')
   .catch(err => console.log(err));
 
 //Settings
-app.set('port', 3000);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
